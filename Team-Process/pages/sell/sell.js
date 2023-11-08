6154f427fd5317475e990cd3e75d534da1c050ef
dai102101211 <1475288793@qq.com>
2023-11-08 16:24:27 +0800

    修改出售

--- Team-Process/pages/sell/sell.js
+++ Team-Process/pages/sell/sell.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imageUrls:["/image/矩形 58.png","/image/图像 1.png"]
+    imageUrls:["/image/矩形 58.png","/image/图像 1.png"],
+    name:'',
+    money:0,
+    message:'无',
+    contact:'无'
   },
 /*传入图片*/
 choosemage: function() {
@@ -20,15 +24,34 @@ choosemage: function() {
     }
   });
 },
-
+/*输入信息*/
+input(ev)
+{
+  var id=ev.currentTarget.id;
+  if(id=='name')
+  {this.data.name=ev.detail.value;}
+  else if(id=='money')
+  {this.data.money=ev.detail.value;}
+  else if(id=='message')
+  {this.data.message=ev.detail.value;}
+  else if(id=='contact')
+  {this.data.contact=ev.detail.value;}
+},
   /*确定信息，并传入后端 */
 ok()
 {
+  var name=this.data.name;
+  var money=this.data.money;
+  var message=this.data.message;
+  var contact=this.data.contact;
+  console.log(name,1111);
+  console.log(money,222);
+  console.log(message,333);
+  console.log(contact,444);
   wx.request({
     url:"", 
     method: 'POST',
     data: {
-      RobotTou:this.data.
     },
     header: {
       'content-type': 'application/json' // 默认值
@@ -37,7 +60,6 @@ ok()
       if(res.statusCode == 200) {
         console.log('用户信息已成功保存到后端');
         console.log(res)//请求成功后的res
-        
       } else {
         console.error('保存到后端失败:', res);
       }
